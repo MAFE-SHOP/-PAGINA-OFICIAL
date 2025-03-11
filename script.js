7f57d9c681a027d18c40e5fd6644cb2637adeab2
MAFE-SHOP <Mafe.ventas.ar@gmail.com>
2025-03-11 10:31:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -199,3 +199,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+//Desplazamiento suave enlaces internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+  
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+  });
+
